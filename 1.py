af97ee6204457f67bac43ccf32093342f2eec61c
wejengin2 <48154165+wejengin2@users.noreply.github.com>
2023-02-05 00:15:33 +0100

    basic chart and data generation

--- 1.py
+++ 1.py
@@ -1,7 +1,50 @@
 import streamlit as st
-import pandas as pd
+import helper as h
+import altair as alt
+###configs
+st.set_page_config(layout="wide", page_title="Weight Tracker")
 
-st.write('')
+###vars
 
-st.title("Streamlit CSV Creation and Download")
-st.write("Enter values for the CSV data and click the 'Create CSV' button.")
+###widgets
+
+def main_chart():
+    min_y = round(st.session_state['weight_history']['weight'].min()) - 1
+    max_y = round(st.session_state['weight_history']['weight'].max()) + 1
+    chart = alt.Chart(st.session_state['weight_history']).mark_line().encode(
+        x=alt.X('date', axis=alt.Axis(title='Date')),
+        y=alt.Y('weight', scale=alt.Scale(domain=(min_y, max_y)), axis=alt.Axis(title='Weight'))
+    )
+    st.write('Weight history chart:')
+    st.altair_chart(chart, use_container_width=True)
+
+def widget_get_data():
+    st.write('Upload your file or create a new one:')
+    uploaded_file = st.file_uploader('',type={"csv"})
+    if uploaded_file is not None:
+        st.session_state['weight_history'] = h.get_csv(uploaded_file)
+    #if st.button('Create a new file'):
+    #    st.session_state['weight_history'] = h.make_new()
+    if st.button('Create a random weight history'):
+        st.session_state['weight_history'] = h.sample_weight(50, 150, 400)
+    
+def widget_reset_data():
+    if st.button('Reset table data'):
+        try:
+            del st.session_state['weight_history']
+        except:
+            st.write('No data to remove.')
+
+###sidebar
+
+with st.sidebar:
+    widget_get_data()
+    widget_reset_data()
+
+#page content
+
+st.title("Weight Tracker")
+
+if 'weight_history' in st.session_state:
+    #st.line_chart(st.session_state['weight_history'], x = 'date')
+    main_chart()
--- helper.py
+++ helper.py
@@ -1,2 +1,40 @@
-import os
 import pandas as pd
+import random
+from datetime import date, timedelta
+
+today = date.today()
+
+def make_new():
+    df = pd.DataFrame({'date': [today],
+                        'weight': [None]})
+    return df
+
+def get_csv(file):
+    df = pd.read_csv(file,  sep=';')
+    return df
+
+def sample_weight_sim(weight, minweight, maxweight):
+    weight_increment = 2.0
+    weight_change = round(random.uniform(-weight_increment, weight_increment), 2)
+    if weight > maxweight:
+        weight = weight - abs(weight_change)
+    elif weight < minweight:
+        weight = weight + abs(weight_change)
+    else:
+        weight = weight + weight_change
+    return weight
+
+
+def sample_weight(minweight, maxweight, age):
+    weight = round(random.uniform(minweight, maxweight), 2)
+    day = date.today() + timedelta(days = -age)
+    df = pd.DataFrame({'date' : [day] , 'weight': weight})
+    r = 0
+    while day != date.today():
+        day = day + timedelta(days = + 1)
+        weight = sample_weight_sim(df.iloc[r,1], minweight, maxweight)
+        #df.append([day,weight])
+        curr_row = pd.DataFrame({'date' : [day] , 'weight': weight})
+        df = pd.concat([df, curr_row])
+        r += 1
+    return df
